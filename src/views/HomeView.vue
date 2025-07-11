6454cf97aaeda8bb919abb7e50502be9f74a9312
Gus Vieira <57501113+GusViieira@users.noreply.github.com>
2025-07-11 22:02:05 +0100

    ajuste na pagina home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+
 import { userStore } from '@/stores/userStore'
 
+
 const storeUser = userStore()
+
 </script>
 
 <template>
   <div>
     <h1>Olá, {{ storeUser.name }} 👋</h1>
   </div>
+
+
+  <div>
+    <p>Bem-vindo ao SaveCash, a sua plataforma de gestão financeira!</p>
+    <p>Explore as funcionalidades e comece a organizar suas finanças hoje mesmo.</p>
+  </div>
+
+  <v-row>
+    <v-col cols="12" sm="6" md="4" lg="3">
+
+    </v-col>
+  </v-row>
 </template>
