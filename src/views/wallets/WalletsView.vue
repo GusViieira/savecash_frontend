49fe2b28c2255c2174cb4aed4ff123c6ff9db7e3
Danrezendee <danielsouza8325@gmail.com>
2025-07-12 01:17:41 +0100

    novos ajustes

--- src/views/wallets/WalletsView.vue
+++ src/views/wallets/WalletsView.vue
@@ -1,8 +1,49 @@
 <script setup lang="ts" name="WalletsView">
 import { ref } from 'vue'
-import WalletCardView from './detail/WalletCardView.vue';
+import WalletCardView from './detail/WalletCardView.vue'
+import { reactive } from 'vue'
+import type { Pagination } from '@/models/Pagination'
+import type {
+  TransactionDTO,
+  TransactionResponseModel,
+} from '@/models/response/TransactionResponseModel'
+
+import DataTable from '@/components/DataTable.vue'
+
+const aba = ref(1)
+
+const tabs = ref([
+  { value: 1, text: 'DADOS DA CONTA' },
+  { value: 2, text: 'METAS FINANCEIRAS' },
+  { value: 3, text: 'PARTICIPANTES' },
+])
 
 const dialog = ref(false)
+
+const headers = [
+  {
+    key: 'type',
+    sortable: true,
+    title: 'Tipo',
+    align: 'center',
+  },
+  { key: 'date', title: 'Meta', align: 'center', sortable: true },
+  { key: 'description', title: 'Motivo', align: 'right', sortable: false },
+  { key: 'categoryDescription', title: 'Progresso', align: 'right', sortable: false },
+  { key: 'note', title: 'Valor da meta', sortable: false, align: 'right' },
+  { key: 'value', title: 'Valor alcançado', sortable: true, align: 'center' },
+  { key: 'actions', title: 'Ações', sortable: false, align: 'center' },
+]
+
+const state = reactive<state>({
+  items: {} as TransactionResponseModel,
+  item: { loading: false } as TransactionDTO,
+  pagination: {
+    page: 0,
+    size: 10,
+    totalPage: 0,
+  } as Pagination,
+})
 </script>
 
 <template>
@@ -21,16 +62,83 @@ const dialog = ref(false)
       <v-col>
         <v-row>
           <v-col>
-            <WalletCardView>
-            </WalletCardView>
+            <WalletCardView> </WalletCardView>
           </v-col>
           <v-col>
-            <WalletCardView>
-            </WalletCardView>
+            <WalletCardView> </WalletCardView>
+          </v-col>
+          <v-col>
+            <WalletCardView> </WalletCardView>
           </v-col>
         </v-row>
       </v-col>
-      teste
+      <v-row class="mt-12 px-5">
+        <v-col cols="12" sm="12" md="6" lg="12">
+          <v-tabs v-model="aba" color="primary">
+            <v-tab v-for="tab in tabs" :key="tab.value" :value="tab.value">
+              {{ tab.text }}
+            </v-tab>
+          </v-tabs>
+          <!-- Conteúdo dos formulários, um para cada tab -->
+          <div v-if="aba === 1">
+            <v-row class="mt-6">
+              <v-col>
+                <!-- Campos do formulário para DADOS DA CONTA -->
+                <v-text-field label="Nome da Conta" variant="solo-filled" density="compact" />
+                <v-text-field label="Moeda da conta" variant="solo-filled" density="compact" />
+                <v-text-field label="Data de criação" variant="solo-filled" density="compact" />
+              </v-col>
+              <v-col>
+                <v-text-field label="Tipo da conta" variant="solo-filled" density="compact" />
+                <v-text-field label="Status da conta" variant="solo-filled" density="compact" />
+              </v-col>
+            </v-row>
+          </div>
+          <div v-else-if="aba === 2" class="mt-8">
+            <data-table
+              :headers="headers"
+              :items="state.items.transactionDTO"
+              :totalPage="state.items.totalPage"
+              :size="state.pagination.size"
+              :page="state.pagination.page"
+            >
+            </data-table>
+          </div>
+
+          <div v-else-if="aba === 3">
+            <!-- Campos do formulário para PARTICIPANTES -->
+            <v-row class="mt-6">
+              <v-col cols="12" sm="12" md="6" lg="3">
+                <!-- Campos do formulário para DADOS DA CONTA -->
+                <v-text-field
+                  label="Nome do participante"
+                  variant="solo-filled"
+                  density="compact"
+                />
+              </v-col>
+              <v-col>
+                <v-text-field
+                  label="E-mail do participante"
+                  variant="solo-filled"
+                  density="compact"
+                />
+              </v-col>
+              <v-col cols="12" sm="12" md="6" lg="2">
+                <v-text-field label="Tipo de acesso" variant="solo-filled" density="compact" />
+              </v-col>
+              <v-col cols="12" sm="12" md="6" lg="1">
+                <v-icon> mdi-pencil-outline </v-icon>
+              </v-col>
+              <v-col cols="12" sm="12" md="6" lg="1">
+                <v-icon> mdi-delete-outline </v-icon>
+              </v-col>
+              <v-col>
+                <v-icon label="Tipo de acesso" variant="solo-filled" density="compact" />
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
   <v-dialog v-model="dialog" max-width="600" />
--- src/views/wallets/detail/WalletCardView.vue
+++ src/views/wallets/detail/WalletCardView.vue
@@ -1,38 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const saldo = ref(1234.56)
+
+</script>
+
 <template>
-   <v-card
-  class="mx-lg"
-  color="lightGrey"
-  max-width="450"
-  min-height="200"
-  rounded="lg"
->
-  <!-- Cabeçalho com título, subtítulo e ícone -->
-  <v-row class="d-flex justify-space-between align-center pa-4">
-    <v-col cols="12" sm="12" md="6" lg="3" >
-      <div class="text-caption" style="color: #fff; opacity: 0.7;">
-        Conta em grupo
-      </div>
-      <v-card-subtitle class="pa-0" style="color: #fff; opacity: 0.6;">
-        Moeda: EUR
-      </v-card-subtitle>
+  <v-card class="pa-5" color="lightGrey" max-width="440" max-height="250" rounded="lg">
+    <!-- Cabeçalho com título, subtítulo e ícone -->
+    <v-row class="px-2">
+      <v-col cols="12" sm="12" md="6" lg="9">
+        <span>
+          <h3>Conta de Família</h3>
+        </span>
+        <span>
+          <h4>Moeda: EUR</h4>
+        </span>
+      </v-col>
+      <v-col class="mt-n2" cols="12" sm="12" md="6" lg="3">
+        <v-icon style="color: rgba(0, 0, 0, 0.3)" size="115" class="ml-n4">
+          mdi-wallet-outline
+        </v-icon>
+      </v-col>
+    </v-row>
 
-    </v-col>
-    <v-icon color="darkGrey" size="90" style="opacity: 0.6;">mdi-wallet-outline</v-icon>
-  </v-row>
-
-  <!-- Saldo atual destacado -->
-  <v-row class="d-flex justify-end pr-6 pt-2">
-    <v-col style="background: rgba(0,0,0,0.15); border-radius: 8px; padding: 8px 16px;">
-      <div class="text-caption" style="color: #fff; opacity: 0.7;">SALDO ATUAL</div>
-      <div style="color: #4be27e; font-size: 1.3rem; font-weight: bold;">2.890,00</div>
-    </v-col>
-  </v-row>
-
-  <!-- Ações do card -->
-  <template v-slot:actions>
-     <v-card-actions class="pa-5" style="color: #fff; font-size: 1.25rem;">
-        Finanças familiares
-       </v-card-actions>
-  </template>
-</v-card>
+    <!-- Saldo atual destacado -->
+    <v-row>
+      <v-col cols="12" sm="12" md="6" lg="7">
+        <v-card-text class="mt-8">
+          <h4 style="font-weight: bold">Conta: 002000329183</h4>
+        </v-card-text>
+      </v-col>
+      <v-col cols="12" sm="12" md="6" lg="5">
+        <v-card style="background: rgba(0, 0, 0, 0.15)">
+          <v-card-text class="mt-n3">
+            <span style="color: #fff; opacity: 0.7; font-size: 0.7rem"> SALDO ATUAL </span>
+            <div class="text-center mt-2">
+              <span style="color: #4be27e; font-size: 1.2rem; font-weight: bold">
+                {{ saldo.toLocaleString('pt-PT', { style: 'currency', currency: 'EUR' }) }}
+              </span>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
